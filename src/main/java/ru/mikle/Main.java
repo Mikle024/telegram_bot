025c7230d6000c29d0a37b50124e781924295836
Mikhail <mikle0240@gmail.com>
2024-01-29 12:55:34 +0300

    Add Main.java

---
+++ src/main/java/ru/mikle/Main.java
@@ -0,0 +1,13 @@
+package ru.mikle;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import ru.mikle.bot.TelegramBot;
+
+public class Main {
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new TelegramBot());
+    }
+}
