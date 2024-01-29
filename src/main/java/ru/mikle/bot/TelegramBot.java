c64d7ce79347ccfddec10c99815e52e6f1e1a171
Mikhail <mikle0240@gmail.com>
2024-01-29 11:45:55 +0300

    Modified TelegramBot.java, add methods and settings

--- src/main/java/ru/mikle/bot/TelegramBot.java
+++ src/main/java/ru/mikle/bot/TelegramBot.java
@@ -1,21 +1,34 @@
 package ru.mikle.bot;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class TelegramBot extends TelegramLongPollingBot {
     @Override
     public String getBotUsername() {
-        return null;
+        return "bearv_gpt";
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return "6517348933:AAEDiffz31D56XCum4ZoZGSyFqXgitBbp7A";
     }
 
     @Override
     public void onUpdateReceived(Update update) {
+        String chatId = update.getMessage().getChatId().toString();
+        String text = update.getMessage().getText();
 
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.getChatId(chatId);
+        sendMessage.setText(text);
+
+        try {
+            this.execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
